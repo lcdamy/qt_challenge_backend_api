d5ae72bf48d0c70cab5bbd1477f6abf17a06c69c
lcdamy <zudanga@gmail.com>
2025-03-10 08:07:59 +0200

    Change Swagger documentation endpoint to root path

--- src/swagger.config.ts
+++ src/swagger.config.ts
@@ -10,5 +10,5 @@ export function setupSwagger(app: INestApplication): void {
     .addTag('api')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('/', app, document);
 }
